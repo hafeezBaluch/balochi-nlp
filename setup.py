a9a0b6b6836e89910bceeccb26e5ded81167c393
hafeezBaluch <hafeezullahhassan2019.com>
2025-07-15 17:05:34 +0500

    update package version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="balochi-nlp",
-    version="0.1.0",
+    version="1.0.0",
     author="Hafeez Baloch",
     author_email="hafeezullahhassan2019@gmail.com",
     description=(
