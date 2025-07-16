42874b64580e89a833ea74ddb7a047a752a35cf6
hafeezBaluch <hafeezullahhassan2019.com>
2025-07-16 09:46:30 +0500

    Package version updated in pyproject.toml and setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="balochi-nlp",
-    version="1.0.0",
+    version="1.0.1",
     author="Hafeez Baloch",
     author_email="hafeezullahhassan2019@gmail.com",
     description=(
